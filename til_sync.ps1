2503d065ccc9ca91b85645ab4ac40e336db0c0da
Junseo Kim <jookpower1022@gmail.com>
2025-06-21 00:03:32 +0900

    ?봽 TIL ?먮룞 ?숆린?? 2025-06-21 00:03

--- til_sync.ps1
+++ til_sync.ps1
@@ -1,12 +1,17 @@
+# 경로 설정
+chcp 65001 > $null
+$src = 'C:\Users\Gram\Desktop\Para\2 Area\TIL(Today I Leared)'
+$dst = 'C:\Users\Gram\Desktop\TIL-Public'
 
-$src = "C:\Users\Gram\Desktop\Para\2 Area\TIL(Today I Leared)"
-$dst = "C:\Users\Gram\Desktop\TIL-Public"
-
+# 1. 대상 폴더로 파일 복사
 Write-Host "📂 복사 중: $src → $dst"
 Copy-Item "$src\*" "$dst\" -Recurse -Force
 
+# 2. Git 커밋 & 푸시
 Set-Location $dst
 git add .
 $now = Get-Date -Format "yyyy-MM-dd HH:mm"
 git commit -m "🔄 TIL 자동 동기화: $now"
-git push origin main  # or master
+git push origin main
+
+pause
